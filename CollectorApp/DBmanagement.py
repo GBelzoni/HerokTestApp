ecbfaf6d7f2c19f0187d75c0d3ff677eaf059b92
GBelzoni <patrickhcostello@gmail.com>
2013-12-11 22:51:14 +0300

    Batch Collecting running

--- CollectorApp/DBmanagement.py
+++ CollectorApp/DBmanagement.py
@@ -1,95 +1,87 @@
 from dateutil import parser
 import datetime
 
-def setup_sqlalchemy():
-    
-    #Define db model
-    db_name = 'Collector.sqlite'
-    #Create connection to db
-    from sqlalchemy import create_engine
-    engine = create_engine('sqlite:///'+db_name,echo=False)
-    
-    #connect to engine
-    #This next would go where the engine statement is in prod. It is handle to db
-    from sqlalchemy.orm import sessionmaker
-    Session = sessionmaker(bind=engine)
-    session = Session() #The session object is now the handle to our db
+#DBmanagementimports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-    
+# from sqlalchemy.ext.declarative import declarative_base
+# Base = declarative_base()
 
-    return session, engine
-    
+from CollectorApp.SqlalchemyBase import Base
+ 
+class DBmanager(object):
+     
+    def __init__(self, db_name = 'Collector.sqlite'):
+         
+        #Create connection to db        
+        self.engine = create_engine('sqlite:///'+db_name,echo=False)
+        #connect to engine
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session() #The session object is now the handle to our db
+         
+    def create_all(self):
+        #Create the tables in db specified by engine
+        Base.metadata.create_all(self.engine)
+     
+    def add_record(self, table, rc):
+        rc_for_table = table(rc)
+        self.session.merge(rc_for_table) #use merge rather than add to deal with duplicates
+ 
+ 
+#Table Comments imports
 #Import columns and types to make table
 from sqlalchemy import Column, Integer, String, DateTime
-
-#import base class to make table from
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
-
-#Add records to db
 from sqlalchemy.exc import IntegrityError
-    
+     
 #Define class that represents Comments table
 class Comments(Base):
-  __tablename__ = 'comments'
-
-  #Define schema
-  #id = Column(Integer, primary_key=True)
-  page_name = Column(String)
-  page_id = Column(Integer)
-  post_message = Column(String)
-  post_id = Column(Integer)
-  post_created_time = Column(DateTime)
-  post_comments_id = Column(String, primary_key=True)
-  post_comments_message = Column(String)
-  post_comments_from_name = Column(String)
-  post_comments_from_id = Column(String)
-  post_comments_created_time = Column(DateTime) #Column(DateTime(timezone=True))
-
-  #Define
-  def __init__(self, record):
-
-    """
-    Be nice to find a quicker way to do
-    """
-
-
-    self.page_name = record['PageName']
-    self.page_id = record['PageId']
-    self.post_message = record['post_message']
-    self.post_id = record['post_id']
-    self.post_created_time = parser.parse(record['post_created_time'],ignoretz=True)
-    self.post_comments_id = record['post_comments_id']
-    self.post_comments_message = record['post_comments_message']
-    self.post_comments_from_name = record['post_comments_from_name']
-    self.post_comments_from_id = record['post_comments_from_id']
-    self.post_comments_created_time = parser.parse(record['post_comments_created_time'],ignoretz=True)
-
-     
-
-  def __repr__(self):
-    return "<comment('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')>" % (self.page_name,
-                                                                   self.page_id,
-                                                                   self.post_message,
-                                                                   self.post_id,
-                                                                   self.post_created_time,
-                                                                   self.post_comments_id,
-                                                                   self.post_comments_message,
-                                                                   self.post_comments_from_name,
-                                                                   self.post_comments_from_id,
-                                                                   self.post_comments_created_time)
-
-
-def create_all(engine):
-    #Create the tables in db specified by engine
-    Base.metadata.create_all(engine)
-
-
-def add_record(rc,session):
-    rc_comments = Comments(rc)
-    session.merge(rc_comments) #use merge rather than add to deal with duplicates
-
-
+    __tablename__ = 'comments'
+    
+    #Define schema
+    #id = Column(Integer, primary_key=True)
+    page_name = Column(String)
+    page_id = Column(Integer)
+    post_message = Column(String)
+    post_id = Column(Integer)
+    post_created_time = Column(DateTime)
+    post_comments_id = Column(String, primary_key=True)
+    post_comments_message = Column(String)
+    post_comments_from_name = Column(String)
+    post_comments_from_id = Column(String)
+    post_comments_created_time = Column(DateTime) #Column(DateTime(timezone=True))
+    
+    #Define
+    def __init__(self, record):
+        
+        """
+        Be nice to find a quicker way to do
+        """
+        
+        
+        self.page_name = record['PageName']
+        self.page_id = record['PageId']
+        self.post_message = record['post_message']
+        self.post_id = record['post_id']
+        self.post_created_time = parser.parse(record['post_created_time'],ignoretz=True) #You need to ignore tz or when comparing on merge sqlalchemy get confused
+        self.post_comments_id = record['post_comments_id']
+        self.post_comments_message = record['post_comments_message']
+        self.post_comments_from_name = record['post_comments_from_name']
+        self.post_comments_from_id = record['post_comments_from_id']
+        self.post_comments_created_time = parser.parse(record['post_comments_created_time'],ignoretz=True)
+        
+  
+ 
+    def __repr__(self):
+        return "<comment('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')>" % (self.page_name,
+                                                                       self.page_id,
+                                                                       self.post_message,
+                                                                       self.post_id,
+                                                                       self.post_created_time,
+                                                                       self.post_comments_id,
+                                                                       self.post_comments_message,
+                                                                       self.post_comments_from_name,
+                                                                       self.post_comments_from_id,
+                                                                       self.post_comments_created_time)
 
 
--- CollectorApp/FB_collector_source.py
+++ CollectorApp/FB_collector_source.py
@@ -1,20 +1,120 @@
+import datetime
 import os
-collector_wd = os.path.dirname(os.path.realpath(__file__))
-collector_wd = '/home/phcostello/git/HerokTestApp'
-os.chdir(collector_wd)
 import logging
 import datetime
-from CollectorApp.DBmanagement import *
-
-
-##DB management
-session, engine = setup_sqlalchemy()
-create_all(engine)
 import pickle
+import time
+
+#collector_wd = os.path.dirname(os.path.realpath(__file__))
+collector_wd = '/home/phcostello/git/HerokTestApp'
+os.chdir(collector_wd)
+
+#for ipython console
+#%cpaste 
+def main( collect_from, collect_to = datetime.datetime.now()):
+
+    
+    #local imports
+    from CollectorApp.DBmanagement import DBmanager
+    from CollectorApp.DBmanagement import Comments
+    import CollectorApp.fb_query as fb_query
+#     reload(fb_query)
+    
+    ##DB management
+    dbm = DBmanager('Collector.sqlite')
+    dbm.create_all()
+    
+    
+    #Setup collection window params
+    latest_time = int(time.mktime(collect_to.timetuple()))
+    window_delta = collect_to - collect_from
+    
+    
+    #Save latest collected to
+    with  open('CollectorApp/latest_time.txt','w') as f:
+        f.write(str(latest_time))
+    
+    #Setup fbquery object - this needs the window to collect data
+    fb1 = fb_query.fb_query(end_upd_window = collect_to,
+                            start_upd_offset = window_delta)
+    
+    #Get Source List
+    import pandas as pd
+    df = pd.read_csv('CollectorApp/UmatiSources_firstClean.csv')
+    Names = df['Name of Site/Page'].values.tolist()
+    URLs = df['FacebookURL'].values.tolist()
+    page_infos = zip(Names,URLs)
+    page_infos = [ {'page_name' : it[0], 'url' : it[1]} for it in page_infos]
+    
+    #Setup logging
+    #TODO have to fix logging
+    logging.basicConfig(filename='main.log', filemode='w',level=logging.DEBUG)
+    
+    #Setup FB fields to query for comments - look at FB Graph API explorer for possibilities
+    top_level_fields = ['feed','posts']
 
 
-#Get Source List
-import pandas as pd
+    #Collection loop
+    start_time = time.time() #Use this to time data collecting execution
+    i=0
+    #KTNKenya is row 44
+    #Caroling Mutoko is row 23
+    for it in page_infos:
+        log_string = "Name: {0} , url: {1}".format(it['page_name'],it['url'])
+        try:
+            fb1.do_id_query(it,top_level_fields)
+            print 'success for id query {0}, row {1}'.format(it['page_name'],i)
+            logging.info( "Success for id query " + log_string +", row {}".format(i))
+            try:
+                fb1.do_comments_query(300)
+                print "Sucess for comment read for page_name {0} row {1}".format(it['page_name'],i) 
+                logging.info( "Sucess for comment read for page_name {0} row {1}".format(it['page_name'],i))
+                record_list = fb1.to_records()
+                print "converted to records"
+#                 pickle_name = 'PickleJar/Pickle'+it['page_name']+'.pkl'
+                #Pickle download results for testing
+#                 out_pkl = open(pickle_name,'wb')
+#                 pickle.dump(record_list,out_pkl)
+#                 out_pkl.close()
+                for rc in record_list:
+                    dbm.add_record(Comments,rc)
+                try:
+                    dbm.session.commit()
+                    print "Success adding records for {}".format(it['url'])
+                except:
+                    print 'error with db commit'
+                    dbm.session.rollback()
+                i+=1
+            except:
+                print "Error for comment read or conver to record for page_name {0} row {1}".format(it['page_name'],i) 
+                logging.info( "Error for comment read for page_name {0} row {1}".format(it['page_name'],i))
+                i+=1
+        except:
+            print 'Error for id query {0}, row {1}'.format(it['page_name'],i)
+            logging.warning( "Failure for id query " + log_string +", row {}".format(i))
+            i+=1
+    
+    time_taken = time.time() - start_time
+    print "Time taken is ", time_taken/60.0, "minutes"
+
+if __name__ == '__main__':
+    collect_dates = [[datetime.datetime.now() - datetime.timedelta(hours=(j+1)),
+                     datetime.datetime.now() - datetime.timedelta(hours=j) ]for j in range(0,12)]
+    
+    for date in collect_dates:
+        main(date[0], date[1])
+
+
+####### Below is miscellaneous code I'm testing
+
+# ##DB management
+# import os
+# collector_wd = '/home/phcostello/git/HerokTestApp'
+# os.chdir(collector_wd)
+# from CollectorApp.DBmanagement import DBmanager
+# dbm = DBmanager()
+# dbm.create_all()
+
 
 # #Get latest post time
 # post_times = session.query(Comments.post_created_time)
@@ -23,126 +123,49 @@ import pandas as pd
 # post_datetimes
 
 
-import CollectorApp.fb_query as fb_query
-reload(fb_query)
 
-# max(post_datetimes)
-datetime.datetime.now()
-end_window_time = datetime.datetime.now()
-# window_delta = datetime.datetime.now() - post_datetimes
-import time
-window_delta = datetime.timedelta(hours =24 )
-latest_time = int(time.mktime(end_window_time.timetuple()))
-
-with  open('CollectorApp/latest_time.txt','w') as f:
-    f.write(str(latest_time))
-
-#Setup fbquery object
-
-fb1 = fb_query.fb_query(end_upd_window = end_window_time,
-                        start_upd_offset = window_delta)
-fb1.access_token
-
-df = pd.read_csv('CollectorApp/UmatiSources_firstClean.csv')
-df
-Names = df['Name of Site/Page'].values.tolist()
-URLs = df['FacebookURL'].values.tolist()
-
-page_infos = zip(Names,URLs)
-page_infos = [ {'page_name' : it[0], 'url' : it[1]} for it in page_infos]
-logging.basicConfig(filename='main.log', filemode='w',level=logging.DEBUG)
-
-top_level_fields = ['feed','posts']
-
-start_time = time.time()
-i=0
-#KTNKenya is row 44
-#Caroling Mutoko is row 23
-# for it in page_infos:
-#     log_string = "Name: {0} , url: {1}".format(it['page_name'],it['url'])
-#     try:
-#         fb1.do_id_query(it,top_level_fields)
-#         print 'success for id query {0}, row {1}'.format(it['page_name'],i)
-#         logging.info( "Success for id query " + log_string +", row {}".format(i))
-#         try:
-#             fb1.do_comments_query(300)
-#             print "Sucess for comment read for page_name {0} row {1}".format(it['page_name'],i) 
-#             logging.info( "Sucess for comment read for page_name {0} row {1}".format(it['page_name'],i))
-#             record_list = fb1.to_records()
-#             print "converted to records"
-#             pickle_name = 'PickleJar/Pickle'+it['page_name']+'.pkl'
-#             #Pickle download results for testing
-#             out_pkl = open(pickle_name,'wb')
-#             pickle.dump(record_list,out_pkl)
-#             out_pkl.close()
-#             for rc in record_list:
-#                 add_record(rc,session)
-#             try:
-#                 session.commit()
-#                 print "Success adding records for {}".format(it['url'])
-#             except:
-#                 print 'error with db commit'
-#                 session.rollback()
-#             i+=1
-#         except:
-#             print "Error for comment read or conver to record for page_name {0} row {1}".format(it['page_name'],i) 
-#             logging.info( "Error for comment read for page_name {0} row {1}".format(it['page_name'],i))
-#             i+=1
-#     except:
-#         print 'Error for id query {0}, row {1}'.format(it['page_name'],i)
-#         logging.warning( "Failure for id query " + log_string +", row {}".format(i))
-#         i+=1
-# 
-# time_taken = time.time() - start_time
-
-
-
-# 
-# print "Time taken is ", time_taken/60.0, "minutes"
-# session.rollback()
-# 
+#Adding records to db 
 # for i in range(0, len(record_list)):
 #     print i
 #     add_record(record_list[i],session)
-# 
-# add_record(record_list[3],session)
+#  add_record(record_list[3],session)
 # len(record_list)
 # session.rollback()
 # fb1.post_ids
 # for i in range(0, len(record_list)):
 #     print record_list[i]['post_comments_message']
-#     
-
-
-#Load saved data
-pickles = os.listdir('PickleJar')
-pickles[1]#KTN Kenya
-rcds = pickle.load(open('PickleJar/'+pickles[1]))
-
-i=0
-this_time = rcds[0]['post_comments_created_time']
-
-from dateutil import parser
-dt = parser.parse(this_time, ignoretz= True)
-dt = dt.replace(tzinfo = None)
-
-
-for pk in pickles:
-    fil = open('PickleJar/'+pk)
-    rcds = pickle.load(fil)
-    fil.close()
-    print pk
-    for i in range(0, len(rcds)):
-        add_record(rcds[i],session)
-
-session.commit()
-
+         
     
-for i in rcds(0, len(rcds)):
-    print rcds[i]['post_comments_message']
+    
+#Load saved data from pickles and examine
+# pickles = os.listdir('PickleJar')
+# pickles[1]#KTN Kenya
+# rcds = pickle.load(open('PickleJar/'+pickles[1]))
+# 
+# i=0
+# this_time = rcds[0]['post_comments_created_time']
+# 
+# from dateutil import parser
+# dt = parser.parse(this_time, ignoretz= True)
+# dt = dt.replace(tzinfo = None)
+# 
+# 
+# for pk in pickles:
+#     fil = open('PickleJar/'+pk)
+#     rcds = pickle.load(fil)
+#     fil.close()
+#     print pk
+#     for i in range(0, len(rcds)):
+#         add_record(rcds[i],session)
+# 
+# session.commit()
+# 
+#     
+# for i in rcds(0, len(rcds)):
+#     print rcds[i]['post_comments_message']
 
 
-
-query = session.query(Comments.page_name)
-query = session.query(Comments.page_name).filter_by(page_name = 'Caroline Mutoko')
-len(set(query.all()))
+#querying db
+# query = session.query(Comments.page_name)
+# query = session.query(Comments.page_name).filter_by(page_name = 'Caroline Mutoko')
+# len(set(query.all()))
